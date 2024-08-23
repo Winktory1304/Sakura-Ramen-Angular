0321d45d38a0e6f7d617cc1e9184b6fb376249d8
Winktory1304 <thomaswinkler88@googlemail.com>
2024-08-23 18:56:35 +0200

    new order

---
+++ src/app/main-content/our-menu.component/our-menu.component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-menu.component',
+  standalone: true,
+  imports: [],
+  templateUrl: './our-menu.component.component.html',
+  styleUrl: './our-menu.component.component.scss'
+})
+export class OurMenuComponentComponent {
+
+}
