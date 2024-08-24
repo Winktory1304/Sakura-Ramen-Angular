93b51e971809b0703b90ca5f2ea2f12940a14bd5
Winktory1304 <thomaswinkler88@googlemail.com>
2024-08-24 10:39:05 +0200

    added imprints

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from "@angular/common";
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    MainContentComponent,
+    FooterComponent,
+    ImprintComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
